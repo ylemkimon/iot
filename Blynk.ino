388ddc09828672605da5dc095d25e0ce86e2394f
ylemkimon <wisechild5@naver.com>
2017-02-18 23:17:05 +0900

    Add files via upload

--- Blynk.ino
+++ Blynk.ino
@@ -2,7 +2,7 @@
 
 #include "blynk.h"
 
-char auth[] = "AUTH";
+char auth[] = "(Token)";
 
 int mainLight = D1;
 int auxLight = D0;
@@ -47,6 +47,19 @@ BLYNK_WRITE(V1) {
 BLYNK_WRITE(V2) {
     digitalWrite(auxLight, param.asInt());
     Blynk.virtualWrite(V0, digitalRead(mainLight) + digitalRead(auxLight) < 2);
+}
+
+// ZeRGBa
+BLYNK_WRITE(V4) {
+    int r = param[0].asInt();
+    int g = param[1].asInt();
+    int b = param[2].asInt();
+    if (r < 255 || g < 255 || b < 255) {
+        RGB.control(true);
+        RGB.color(r, g, b);
+    } else {
+        RGB.control(false);
+    }
 }
 
 void loop() {
